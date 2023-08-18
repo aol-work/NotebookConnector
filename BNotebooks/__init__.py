c70398559e197afae66d9da269318dd54921bb1f
Ali Soufali <49349288+alisoufali@users.noreply.github.com>
2023-08-18 12:45:19 +0200

    Formatted the code to follow PEP8 guidelines.

--- BNotebooks/__init__.py
+++ BNotebooks/__init__.py
@@ -1,3 +1,9 @@
+import bpy
+from bpy.utils import register_class, unregister_class
+
+from . import pkg, pref
+
+
 bl_info = {
     "name": "BNotebooks",
     "author": "Brady Johnston",
@@ -10,11 +16,6 @@ bl_info = {
     "category": "Development",
     }
 
-import bpy
-from bpy.utils import register_class, unregister_class
-from . import pkg
-from . import pref
-
 class_list = (
     pref.BNotebooksPreferences,
     pref.BN_Kernel_Append,
@@ -22,8 +23,10 @@ class_list = (
     pkg.MOL_OT_Install_Package
 )
 
+
 def get_mirror_items(scene, context):
     return [(key, key, "") for key in pkg.PYPI_MIRROR.keys()]
+
 
 def register():
     bpy.types.Scene.pypi_mirror_provider = bpy.props.EnumProperty(
@@ -33,6 +36,7 @@ def register():
     )
     for c in class_list:
         register_class(c)
+
 
 def unregister():
     del bpy.types.Scene.pypi_mirror_provider
